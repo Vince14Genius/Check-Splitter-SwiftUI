bfc0c4f6224980f59d60afd1d9af8536ad8b9244
Vince14Genius <vince14genius@icloud.com>
2021-09-18 23:29:08 -0400

    Renamed types, added docs, and added support for remainders

--- Check Splitter Shared/Model.swift
+++ Check Splitter Shared/Model.swift
@@ -8,10 +8,10 @@
 /*
 Independent Variables:
 - totalPayment : Amount
-- rawAmounts : [RawAmount] = [payerAmount1, payerAmount2, ..., payerAmountN]
+- rawAmounts : [LabeledAmounts] = [payerAmount1, payerAmount2, ..., payerAmountN]
 
-Structure of RawAmount:
-- RawAmount : [ItemName: Amount] = [itemPrice1, itemPrice2, ..., itemPriceN]
+Structure of LabeledAmounts:
+- LabeledAmounts : [ItemName: Amount] = [itemPrice1, itemPrice2, ..., itemPriceN]
 
 Dependent Variables:
 - finalAmounts : [Amount] [finalAmount1, finalAmount2, ..., finalAmountN]
@@ -21,13 +21,14 @@ import Foundation
 
 public typealias PayerName = String
 public typealias ItemName = String
-public typealias RawAmount = [ItemName: Amount]
+public typealias LabeledAmounts = [ItemName: Amount]
+public typealias SplitResult = (result: [PayerName: Amount], remainder: Amount)
 
-fileprivate func payerRawSum(_ rawAmount: RawAmount) -> Amount {
+fileprivate func payerRawSum(_ rawAmount: LabeledAmounts) -> Amount {
     return rawAmount.reduce(0, { $0 + $1.value })
 }
 
-fileprivate func reduceRaw(payers: [PayerName: RawAmount]) -> [PayerName: Amount] {
+fileprivate func reduceRaw(payers: [PayerName: LabeledAmounts]) -> [PayerName: Amount] {
     return payers.mapValues(payerRawSum)
 }
 
@@ -36,7 +37,20 @@ fileprivate func payerProportions(payers: [PayerName: Amount]) -> [PayerName: Do
     return payers.mapValues { $0 / total }
 }
 
-public func splitCheck(totalPayment: Amount, payers: [PayerName: RawAmount]) -> [PayerName: Amount] {
+/**
+ Call this function to split the check.
+ 
+ - parameters:
+    - totalPayment: the total payment to divide from
+    - payers: a `Dictionary` consisting of the subtotals assigned to each payer
+ 
+ - returns: a `Dictionary` consisting of shares of the total payment assigned to each payer, based on the proportions of each payer's subtotal in relation to the sum of subtotals
+ */
+public func splitCheck(totalPayment: Amount, payers: [PayerName: LabeledAmounts]) -> SplitResult {
     let proportions = payerProportions(payers: reduceRaw(payers: payers))
-    return proportions.mapValues { Amount($0) * totalPayment }
+    let result = proportions.mapValues { Amount($0) * totalPayment }
+    return (
+        result: result,
+        remainder: totalPayment - result.reduce(0, { $0 + $1.value })
+    )
 }
