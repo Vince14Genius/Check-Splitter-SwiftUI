b14819e960a2d5c9675ee8c82bba3e2b9484f50f
Vince14Genius <vince14genius@icloud.com>
2021-09-20 16:01:52 -0400

    a

---
+++ Check Splitter iOS/CheckSplitterApp.swift
@@ -0,0 +1,28 @@
+//
+//  CheckSplitterApp.swift
+//  Check Splitter iOS
+//
+//  Created by Vincent C. on 9/18/21.
+//
+
+import SwiftUI
+
+@main
+struct CheckSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                WelcomeView()
+            }
+        }
+    }
+}
+
+struct CheckSplitterApp_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            WelcomeView()
+        }
+    }
+}
+
