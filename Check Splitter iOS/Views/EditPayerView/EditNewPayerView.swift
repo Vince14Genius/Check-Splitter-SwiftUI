dfbc1a49f0dd93a962beb40a2102af1c6d80335c
Vince14Genius <vince14genius@icloud.com>
2021-10-07 12:56:37 -0700

    Added "edit payer right after creation" feature

---
+++ Check Splitter iOS/Views/EditPayerView/EditNewPayerView.swift
@@ -0,0 +1,34 @@
+//
+//  EditNewPayerView.swift
+//  Check Splitter
+//
+//  Created by Vincent C. on 10/6/21.
+//
+
+import SwiftUI
+
+struct EditNewPayerView: View {
+    @Binding var payers: [PayerWithItems]
+    @State var newPayerId: UUID?
+    
+    var body: some View {
+        if let id = newPayerId {
+            EditPayerView(payerToEdit: $payers[index(withID: id, in: payers)])
+        } else {
+            Text("Creating new payer...")
+                .onAppear {
+                    let newPayer = PayerWithItems(name: "", items: [])
+                    newPayerId = newPayer.id
+                    self.payers.append(newPayer)
+                }
+        }
+    }
+}
+
+struct EditNewPayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            EditNewPayerView(payers: .constant([]))
+        }
+    }
+}
